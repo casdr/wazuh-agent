0b062b7bccde9982faa8620e2b096519f2da0e96
jr0me <joaquin.romera@wazuh.com>
2024-08-09 19:44:21 +0200

    temp
    notes:
    
    Queue header has the same name as others, namely queue.hpp from boost this
    causes intellisense to fail to recognize symbols, bigger problems may arise.
    
    Some necessary changes to cmakes were made to allow compilation, aside
    from the queue header name, the cmake wasnt generating, some linking
    must be made public.
    
    Currently experimenting passing a lambda to the communicator with the
    strategy on how to get a message to send. This way the communciator
    can be kept decoupled from the queue implementation.

--- src/agent/src/agent.cpp
+++ src/agent/src/agent.cpp
@@ -1,6 +1,34 @@
 #include <agent.hpp>
 
+#include <shared.hpp>
+
+#include <string>
 #include <thread>
+
+namespace
+{
+    template<MessageType T>
+    auto getMessagesFromQueue(MultiTypeQueue& multiTypeQueue)
+    {
+        return [&multiTypeQueue]() -> boost::asio::awaitable<std::string>
+        {
+            std::cout << "Getting messages from queue\n";
+            const auto message = co_await multiTypeQueue.getNextAwaitable(T);
+            co_return message.data.dump();
+        };
+    }
+
+    template<MessageType T>
+    auto popMessagesFromQueue(MultiTypeQueue& multiTypeQueue)
+    {
+        return [&multiTypeQueue](const std::string& response) -> void
+        {
+            std::cout << "Response: " << response << '\n';
+            std::cout << "Popping messages from queue\n";
+            multiTypeQueue.pop(T);
+        };
+    }
+} // namespace
 
 Agent::Agent()
     : m_communicator(m_agentInfo.GetUUID(),
@@ -18,9 +46,12 @@ Agent::~Agent()
 void Agent::Run()
 {
     m_taskManager.EnqueueTask(m_communicator.WaitForTokenExpirationAndAuthenticate());
-    m_taskManager.EnqueueTask(m_communicator.GetCommandsFromManager(m_messageQueue));
-    m_taskManager.EnqueueTask(m_communicator.StatefulMessageProcessingTask(m_messageQueue));
-    m_taskManager.EnqueueTask(m_communicator.StatelessMessageProcessingTask(m_messageQueue));
+    m_taskManager.EnqueueTask(m_communicator.GetCommandsFromManager(getMessagesFromQueue<COMMAND>(m_messageQueue),
+                                                                    popMessagesFromQueue<COMMAND>(m_messageQueue)));
+    m_taskManager.EnqueueTask(m_communicator.StatefulMessageProcessingTask(
+        getMessagesFromQueue<STATEFUL>(m_messageQueue), popMessagesFromQueue<STATEFUL>(m_messageQueue)));
+    m_taskManager.EnqueueTask(m_communicator.StatelessMessageProcessingTask(
+        getMessagesFromQueue<STATELESS>(m_messageQueue), popMessagesFromQueue<STATELESS>(m_messageQueue)));
 
     m_signalHandler.WaitForSignal();
 }
